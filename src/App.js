1385e329f2a949b8d56b538fa253544540210162
Konrad Bauer <konradbauer94@gmail.com>
2022-09-24 01:16:37 +0200

    code format added

--- src/App.js
+++ src/App.js
@@ -6,31 +6,26 @@ import Header from "./Header";
 import Container from "./Container";
 
 const tasks = [
-  {id: 1, content: "przejść na Reacta", done: false},
-  {id: 2, content: "zjeść kolację", done: true},
+  { id: 1, content: "przejść na Reacta", done: false },
+  { id: 2, content: "zjeść kolację", done: true },
 ];
 
 const hideDoneTasks = false;
 
 function App() {
   return (
-      <Container>
-        <Header title="Lista zadań"/>
-        <Section 
-          title= "Dodaj nowe zadanie" 
-          body={<Form />} 
-        />
-        
-        <Section 
-          title= "Lista zadań" 
-          body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks}
-        />
-        
+    <Container>
+      <Header title="Lista zadań" />
+      <Section title="Dodaj nowe zadanie" body={<Form />} />
+
+      <Section
+        title="Lista zadań"
+        body={<Tasks tasks={tasks} hideDoneTasks={hideDoneTasks} />}
+        extraHeaderContent={
+          <Buttons tasks={tasks} hideDoneTasks={hideDoneTasks} />
         }
-          extraHeaderContent={<Buttons tasks={tasks} hideDoneTasks={hideDoneTasks}/>
-        }  
-        />
-      </Container>
+      />
+    </Container>
   );
 }
 
--- src/Tasks/index.js
+++ src/Tasks/index.js
@@ -1,17 +1,20 @@
 import "./style.css";
 
-const Tasks = ({tasks, hideDoneTasks}) => (
-    <ul className="tasks section__list">
-        {tasks.map(task => (
-            <li key={task.id}
-            className={`task${task.done && hideDoneTasks ? " task--hidden" : ""}`}
-            >
-            <button className="button ">{task.done ? "✔" : ""}</button>  
-            <span className={`task${task.done ? " task--done" : ""}`}>{task.content}</span>
-            <button className="button--remove">🗑️</button>
-            </li>
-        ))}
-    </ul>
+const Tasks = ({ tasks, hideDoneTasks }) => (
+  <ul className="tasks section__list">
+    {tasks.map((task) => (
+      <li
+        key={task.id}
+        className={`task${task.done && hideDoneTasks ? " task--hidden" : ""}`}
+      >
+        <button className="button ">{task.done ? "✔" : ""}</button>
+        <span className={`task${task.done ? " task--done" : ""}`}>
+          {task.content}
+        </span>
+        <button className="button--remove">🗑️</button>
+      </li>
+    ))}
+  </ul>
 );
 
 export default Tasks;
