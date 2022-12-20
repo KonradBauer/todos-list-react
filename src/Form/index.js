ac023b57fcfd78980ca2c14e6379c07a49245d7d
Konrad Bauer <konradbauer94@gmail.com>
2022-12-20 23:05:50 +0100

    focus input after add task

--- src/Form/index.js
+++ src/Form/index.js
@@ -1,7 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import "./style.css";
 
-const Form = ({ addNewTask, tasks, toLocalStorage }) => {
+const Form = ({ addNewTask }) => {
   const [newTaskContent, setNewTaskContent] = useState("");
 
   const onFormSubmit = (event) => {
@@ -17,10 +17,16 @@ const Form = ({ addNewTask, tasks, toLocalStorage }) => {
     setNewTaskContent("");
   };
 
+  const inputRef = useRef(null);
+  const inputFocus = () => {
+    inputRef.current.focus();
+  };
+
   return (
     <form className="form" onSubmit={onFormSubmit}>
       <div className="form__container">
         <input
+          ref={inputRef}
           value={newTaskContent}
           className="form__input"
           autoFocus
@@ -28,7 +34,9 @@ const Form = ({ addNewTask, tasks, toLocalStorage }) => {
           placeholder="Co jest do zrobienia?"
           onChange={({ target }) => setNewTaskContent(target.value)}
         />
-        <button className="form__button">Dodaj zadanie</button>
+        <button onClick={inputFocus} className="form__button">
+          Dodaj zadanie
+        </button>
       </div>
     </form>
   );
