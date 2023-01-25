8e7bc96f4259aa084a96aef8c49b62d0db97b672
Konrad Bauer <konradbauer94@gmail.com>
2023-01-25 22:18:50 +0100

    add toggleTaskDone feature

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,15 +1,16 @@
 import { List, Item, Button, Task } from "./styled";
-import { useSelector } from "react-redux";
-import { selectTasks } from "../tasksSlice";
+import { useSelector, useDispatch } from "react-redux";
+import { selectTasks, toggleTaskDone } from "../tasksSlice";
 
-const TaskList = ({ removeTask, toggleTaskDone }) => {
+const TaskList = ({ removeTask }) => {
   const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
 
   return (
     <List>
       {tasks.map((task) => (
         <Item key={task.id} hidden={task.done && hideDone}>
-          <Button onClick={() => toggleTaskDone(task.id)}>
+          <Button onClick={() => dispatch(toggleTaskDone(task.id))}>
             {task.done ? "✔" : ""}
           </Button>
           <Task done={task.done}>{task.content}</Task>
