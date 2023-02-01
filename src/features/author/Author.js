dd73ce3dd8d159f8cd4be9ba75d0d76e73b5e703
Konrad Bauer <konradbauer94@gmail.com>
2023-02-01 22:07:38 +0100

    added author photo

--- src/features/author/Author.js
+++ src/features/author/Author.js
@@ -1,6 +1,7 @@
 import { Container } from "../../common/Container/styled";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
+import photo from "../../images/photo.png";
 
 export default () => (
   <Container>
@@ -8,11 +9,17 @@ export default () => (
     <Section
       title="Konrad Bauer"
       body={
-        <p>
-          Hej! Mam na imię Konrad i mam 28 lat. Jestem z wykształcenia muzykiem,
-          gram głównie na klarnecie oraz na gitarze. Oprócz tego interesuję się
-          gotowaniem, astronomią, e-sportem i inwestycjami na giełdzie.
-        </p>
+        <>
+          <p>
+            <img src={photo} alt="Konrad Bauer" />
+          </p>
+          <p>
+            Hej! Mam na imię Konrad i mam 28 lat. Jestem z wykształcenia
+            muzykiem, gram głównie na klarnecie oraz na gitarze. Oprócz tego
+            interesuję się gotowaniem, astronomią, e-sportem i inwestycjami na
+            giełdzie.
+          </p>
+        </>
       }
     />
   </Container>
